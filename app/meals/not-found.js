24e09121d72866fbf9749f96b3a0b84453310f4d
Jan Illetško <jan.illetsko@greyson.eu>
2024-04-11 14:06:12 +0200

    initial commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the requested meal data.</p>
+    </main>
+  );
+}
